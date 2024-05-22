7f1a69ea5f5442f3eac8d32d37a8d671b66f9f6f
jigoo <165612433+jisu0212@users.noreply.github.com>
2024-05-22 22:41:35 +0900

    체중계

--- project/script/diet.js
+++ project/script/diet.js
@@ -4,3 +4,12 @@
 - 버튼을 눌렀을 때 최종 목표 칸에 메세지 출력하기
 결과예시) 적정체중은 ?kg이며 ?kg 초과되셨습니다.
 */
+const height = document.querySelector('#height')
+const weight = document.querySelector('#weight')
+const goals = document.querySelector('#goals')
+const btn = document.querySelector('button')
+btn.addEventListener('click', function(){
+    let total = (Number(height.value) - 100)*0.9;
+    let over_weight = Number(weight.value) - total;
+    goals.value = (`적정체중은 ${total}kg이며 ${over_weight}kg 초과되셨습니다.`)
+})
